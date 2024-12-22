bdc5b1db34fe7c8fd030652351b9d585678bc936
wynehtetaung <wynehtet2022@gmail.com>
2024-12-22 12:30:30 +0630

    [setup] api

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,7 @@
-const a = 12;
+const express = require("express");
+const app = express();
+
+app.listen(process.env.PORT || 3000, (err) => {
+  if (err) console.log(`Error : ${err}`);
+  console.log(`server is running at ${process.env.PORT || 3000}`);
+});
